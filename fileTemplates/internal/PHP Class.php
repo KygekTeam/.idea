bab1232b56480326ce54689f6853732b74c23c5c
KygekDev <68368066+KygekDev@users.noreply.github.com>
2022-01-30 14:53:23 +0800

    Add files

---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,24 @@
+<?php
+#parse("PHP File Header.php")
+
+/*
+ * <Description>
+ * Copyright (C) <Year> KygekTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+declare(strict_types=1);
+
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+#end
+
+class ${NAME} {
+
+
+
+}
